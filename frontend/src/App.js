0556b1776b5a4377e7e3ff65cc7e6ffdd5886329
bumkyu00 <bumkyu00@naver.com>
2022-07-20 00:51:09 +0900

    connect front back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: ""
+  }
+  componentDidMount() {
+    this.getText();
+  }
+  getText() {
+    axios.get('http://localhost:3000/main/')
+      .then(response => this.setState({text: response.data}))
+      .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+        <div>{this.state.text}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
