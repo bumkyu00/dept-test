4cdadef64ab6453ee77013402a7cec135d2742b3
bumkyu00 <bumkyu00@naver.com>
2022-07-21 02:31:18 +0900

    question page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,46 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class App extends Component {
+  questions = [
+    '1. 직업이 있다.',
+    '2. 가족이 있다.'
+  ];
+  answers = [
+    ['예', '아니오', '알바'],
+    ['예', '아니오']
+  ];
+  maxQuestion = 1;
   state = {
-    text: "default"
+    question: 0
   }
-  componentDidMount() {
-    this.getText();
+  // getText() {
+  //   axios.get('/main')
+  //     .then(response => this.setState({text: response.data}))
+  //     .catch(err => console.log(err));
+  // }
+  generateAnswerList() {
+    return this.answers[this.state.question].map((answer) => <button onClick={() => console.log(answer)}>{answer}</button>)
   }
-  getText() {
-    axios.get('/main')
-      .then(response => this.setState({text: response.data}))
-      .catch(err => console.log(err));
+  previousQuestion() {
+    if(this.state.question > 0) {
+      this.setState({question: this.state.question - 1})
+    }
+  }
+  nextQuestion() {
+    if(this.state.question < this.maxQuestion) {
+      this.setState({question: this.state.question + 1})
+    }
   }
   render() {
     return (
       <div>
-        <p>Hi</p>
-        <div>{this.state.text}</div>
+        <p>Question</p>
+        <div>{this.questions[this.state.question]}</div>
+        <div>{this.generateAnswerList()}</div>
+        <div>
+          <button onClick={() => this.previousQuestion()}>뒤로</button>
+          <button onClick={() => this.nextQuestion()}>다음</button>
+        </div>
       </div>
     );
   }
