c648a2027b01de71b3f47b0e16f0c29d5eea0edd
bumkyu00 <bumkyu00@naver.com>
2022-07-23 23:16:14 +0900

    simple design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import './App.css';
+import ProgressBar from './ProgressBar';
 
 class App extends Component {
   questions = [
@@ -10,17 +12,33 @@ class App extends Component {
     ['예', '아니오', '알바'],
     ['예', '아니오']
   ];
-  maxQuestion = 1;
+  numQuestions = 1;
   state = {
-    question: 0
+    question: 0,
+    selected: new Array(this.numQuestions + 1).fill(0)
   }
   // getText() {
   //   axios.get('/main')
   //     .then(response => this.setState({text: response.data}))
   //     .catch(err => console.log(err));
   // }
+  handleRadio(index) {
+    var newSelected = {...this.state.selected};
+    newSelected[this.state.question] = index;
+    this.setState({selected: newSelected});
+  }
   generateAnswerList() {
-    return this.answers[this.state.question].map((answer) => <button onClick={() => console.log(answer)}>{answer}</button>)
+    return this.answers[this.state.question].map((answer, index) => 
+      <div>
+        <input 
+          type='radio' 
+          value={index} 
+          checked={this.state.selected[this.state.question] === index}
+          onClick={() => this.handleRadio(index)}
+        />
+        <label>{answer}</label>
+        </div>
+      )
   }
   previousQuestion() {
     if(this.state.question > 0) {
@@ -28,19 +46,21 @@ class App extends Component {
     }
   }
   nextQuestion() {
-    if(this.state.question < this.maxQuestion) {
+    if(this.state.question < this.numQuestions) {
       this.setState({question: this.state.question + 1})
     }
   }
   render() {
     return (
-      <div>
-        <p>Question</p>
-        <div>{this.questions[this.state.question]}</div>
-        <div>{this.generateAnswerList()}</div>
+      <div className='body'>
         <div>
-          <button onClick={() => this.previousQuestion()}>뒤로</button>
-          <button onClick={() => this.nextQuestion()}>다음</button>
+          <ProgressBar bgcolor='#76BA99' completed={((this.state.question) / (this.numQuestions + 1) * 100)}></ProgressBar>
+        </div>
+        <div className='question'>{this.questions[this.state.question]}</div>
+        <div className='answerList'>{this.generateAnswerList()}</div>
+        <div>
+          <button className='button' onClick={() => this.previousQuestion()}>뒤로</button>
+          <button className='button' onClick={() => this.nextQuestion()}>다음</button>
         </div>
       </div>
     );
---
+++ frontend/src/ProgressBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const ProgressBar = (props) => {
+    const { bgcolor, completed } = props;
+  
+    const containerStyles = {
+      height: 20,
+      width: '80%',
+      marginLeft: '10%',
+      marginRight: '10%',
+      marginTop: 30,
+      backgroundColor: "#e0e0de",
+      borderRadius: 8,
+    }
+  
+    const fillerStyles = {
+      height: '100%',
+      width: `${completed}%`,
+      backgroundColor: bgcolor,
+      borderRadius: 'inherit',
+      textAlign: 'right',
+    }
+  
+    const labelStyles = {
+      padding: 5,
+      color: 'white',
+      fontWeight: 'bold'
+    }
+  
+    return (
+      <div style={containerStyles}>
+        <div style={fillerStyles}>
+          <span style={labelStyles}>{`${completed}%`}</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ProgressBar;
